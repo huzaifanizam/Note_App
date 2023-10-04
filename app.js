a87011d01e5a6c64d6aa0eb59ec24f3820b18647
huzaifanizam <nizamhuzaifa@gmail.com>
2023-10-04 15:48:33 +0500

    first commit

--- app.js
+++ app.js
@@ -56,35 +56,3 @@ document.addEventListener("keydown", event=>{
 
 
 
-
-// function addTask() {
-//     if (notesContainer.value === '') {
-//         alert("You Must Write Something!");
-//     } else {
-//         let li = document.createElement("li");
-//         li.innerHTML = notesContainer.value;
-//         listContainer.appendChild(li);
-//         let span = document.createElement("span");
-//         span.innerHTML = "\u00d7";
-//         li.appendChild(span);
-//     }
-//     notesContainer.value = "";
-//     saveData();
-// }
-// listContainer.addEventListener("click",function(e){
-//     if (e.target.tagName === "LI"){
-//         e.target.classList.toggle("checked")
-//         saveData();
-//     } 
-//     else if(e.target.tagName === "SPAN") {
-//         e.target.parentElement.remove()
-//         saveData()
-//     }
-// } , false);
-
-// function saveData() {
-//     localStorage.setItem("data", listContainer.innerHTML);
-// }
-// function showTask() {
-//     listContainer.innerHTML = localStorage.getItem("data"); 
-// }
